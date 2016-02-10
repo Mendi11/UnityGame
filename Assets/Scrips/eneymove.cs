18713a610234b287e8840cf9d145969d31ceb757
armend.gashi23@hotmail.com <armend.gashi23@hotmail.com>
2016-02-10 19:16:09 +0100

    hej first commit

---
+++ Assets/Scrips/eneymove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eneymove : MonoBehaviour {
+    Transform target;
+    Rigidbody eneRig;
+
+	// Use this for initialization
+	void Start () {
+        eneRig = GetComponent<Rigidbody>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 move = target.position - transform.position;
+        Vector3 vel = eneRig.velocity;
+        vel = move.normalized;
+        eneRig.velocity = vel * 3f;
+
+       // transform.position = Vector3.MoveTowards(transform.position, target.position, 3 * Time.deltaTime);
+    }
+}
---
+++ Assets/Scrips/walk.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class walk : MonoBehaviour {
+
+    private int randomNumber;
+    Rigidbody rgb;
+    //Vector3 playerPos = new Vector3();
+    public GameObject bullets;
+    // Use this for initialization
+    void Start () {
+        rgb = GetComponent<Rigidbody>();
+	
+	}
+    void FixedUpdate()
+    {
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+
+        Vector3 movement = new Vector3(h,0,v);
+
+        Vector3 vel = rgb.velocity;
+        vel = movement.normalized;
+        rgb.velocity = vel * 4f;
+        
+    }
+
+    void OnCollisionEnter(Collision coll) {
+        randomNumber = Random.Range(0,3);
+        print(randomNumber);
+        if (coll.gameObject.tag == "level")
+        UnityEngine.SceneManagement.SceneManager.LoadScene(randomNumber);
+
+    }
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Instantiate(bullets, transform.position, transform.rotation);
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+
+        }
+
+
+    }
+}
