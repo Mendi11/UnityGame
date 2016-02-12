da9433aa384f211856837d78d9a4ff63d1eb6cf1
unknown <lidean@3se-spel-04.utb.fuv.se>
2016-02-12 13:26:28 +0100

    Andreas test

---
+++ Assets/Scrips/And.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class And : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	//asdaopsdkj
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
