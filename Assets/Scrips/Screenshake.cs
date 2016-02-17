6de3a4c8f80f075a60cc9325eaaf59e641c946ba
unknown <lidean@3se-spel-04.utb.fuv.se>
2016-02-17 15:50:56 +0100

    screenshake

---
+++ Assets/Scrips/Screenshake.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Screenshake : MonoBehaviour
+{
+
+    Vector3 originalCameraPosition = new Vector3(-6.09f, 10.95f, -5.38f);
+
+    float shakeAmt = 0;
+    private Camera mainCamera;
+
+    void Awake()
+    {
+
+        mainCamera = Camera.main;
+        shakeAmt = 0.10f;
+        InvokeRepeating("CameraShake", 0, .01f);
+        Invoke("StopShaking", 0.3f);
+    }
+
+    void CameraShake()
+    {
+        if (shakeAmt > 0)
+        {
+            float quakeAmt = Random.value * shakeAmt * 2 - shakeAmt;
+            Vector3 pp = mainCamera.transform.position;
+            pp.y += quakeAmt; // can also add to x and/or z
+            mainCamera.transform.position = pp;
+        }
+    }
+
+    void StopShaking()
+    {
+        CancelInvoke("CameraShake");
+        mainCamera.transform.position = originalCameraPosition;
+
+    }
+
+}
